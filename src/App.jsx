1f7a08c86bcae029f73e81359b9d361c63796235
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-06-25 19:06:40 -0300

    styling 3 ok

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 function App() {
   return (
     <>

6461bac42942d39e4fd2a8d4e8301840de8761d9
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-07-13 09:01:20 -0300

    firebase1 ok

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,17 +1,32 @@
 import { useState, useEffect } from "react"
-import { getUnProducto } from "../../asyncmock"
+// import { getUnProducto } from "../../asyncmock"
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore"
+import { db } from "../../services/config";
 
 const ItemDetailContainer = () => {
   const [producto,setProducto] = useState(null);
   const {idItem} = useParams();
 
   useEffect( () => {
-    getUnProducto(idItem)
-       .then(res => setProducto(res))
-       .catch(error => console.log(error))
-  },[idItem]);
+    const nuevoDoc = doc(db, "inventario", idItem);
+
+    getDoc(nuevoDoc)
+      .then(res => {
+        const data = res.data();
+        const nuevoProducto = {id: res.id, ...data};
+        setProducto(nuevoProducto);
+      })
+      .catch(error => console.log(error))
+  }, [idItem]);
+
+
+  // useEffect( () => {
+  //   getUnProducto(idItem)
+  //      .then(res => setProducto(res))
+  //      .catch(error => console.log(error))
+  // },[idItem]);
 
   return (
     <div>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,20 +1,37 @@
 import "./ItemListContainer.css"
 import { useState, useEffect } from "react"
 import ItemList from "../ItemList/ItemList"
-import { getProductos, getProductosPorCategoria } from "../../asyncmock"
+//import { getProductos, getProductosPorCategoria } from "../../asyncmock"
 import { useParams } from "react-router-dom"
+import { collection, getDocs, query, where } from "firebase/firestore"
+import { db } from "../../services/config"
 
 const ItemListContainer = () => {
   const [productos,setProductos] = useState([]);
 
   const {idCategoria} = useParams();
   
-  useEffect(() => {
-    const funcionProductos = idCategoria ? getProductosPorCategoria : getProductos;
-    funcionProductos(idCategoria)
-      .then(res => setProductos(res))
-      .catch(error => console.log(error))
+  useEffect( () => {
+    const misProductos = idCategoria ? query(collection(db, "inventario"), where
+    ("idCat", "==", idCategoria)) : collection(db, "inventario");
+
+    getDocs(misProductos)
+      .then( res => {
+        const nuevosProductos = res.docs.map (doc => {
+          const data = doc.data();
+          return {id: doc.id, ...data}
+        })
+        setProductos(nuevosProductos);
+      })
+      .catch(error => console.log(error));
   }, [idCategoria])
+
+  // useEffect(() => {
+  //   const funcionProductos = idCategoria ? getProductosPorCategoria : getProductos;
+  //   funcionProductos(idCategoria)
+  //     .then(res => setProductos(res))
+  //     .catch(error => console.log(error))
+  // }, [idCategoria])
 
   return (
     <div>

d56454d7842c18f86d4b4760b1bd05869125b43b
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-07-13 10:50:54 -0300

    add: form ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import Cart from "./components/Cart/Cart";
+import Checkout from "./components/Checkout/Checkout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartContextProvider } from "./context/CartContext";
 import "./App.css"
@@ -17,7 +18,7 @@ function App() {
           <Route path="/categoria/:idCategoria" element={<ItemListContainer/>} />
           <Route path="/item/:idItem" element={<ItemDetailContainer/>} />
           <Route path="/cart" element={<Cart/>} />
-          <Route path="/checkout" element={<p>checkout</p>} />
+          <Route path="/checkout" element={<Checkout/>} />
         </Routes>
         </CartContextProvider>
       </BrowserRouter>
---
+++ src/components/Checkout/Checkout.jsx
@@ -0,0 +1,104 @@
+import { useState, useContext } from "react"
+import { CartContext } from "../../context/CartContext"
+import { db } from "../../services/config"
+import { collection, addDoc } from "firebase/firestore"
+
+
+const Checkout = () => {
+    const { carrito, vaciarCarrito, cantidadTotal } = useContext(CartContext);
+    const [nombre, setNombre] = useState("");
+    const [apellido, setApellido] = useState("");
+    const [telefono, setTelefono] = useState("");
+    const [email, setEmail] = useState("");
+    const [emailConfirmacion, setEmailConfirmacion] = useState("");
+    const [error, setError] = useState("");
+    const [orderId, setOrderId] = useState("");
+
+const manejadorFormulario = (e) => {
+    e.preventDefault();
+    
+    if(!nombre || !apellido || !telefono || !email || !emailConfirmacion) {
+        setError("Por favor, complete todos los campos");
+        return;
+    }
+    if (email !== emailConfirmacion) {
+        setError("Los campos del email no coinciden");
+        return;
+    }
+
+    const orden = {
+        items: carrito.map( producto => ({
+            id: producto.item.id,
+            nombre: producto.item.nombre,
+            cantidad: producto.cantidad
+        })),
+        total: cantidadTotal,
+        nombre,
+        apellido,
+        telefono,
+        email 
+    };
+
+    addDoc(collection (db, "ordenes"), orden)
+        .then(docRef => {
+            setOrderId(docRef.id);
+            vaciarCarrito();
+        })
+        .catch(error => {
+            console.log("Error al crear la orden: ", error);
+            setError("Se produjo un error al crear la orden. Vuelva a intentar en unos segundos")
+        })
+}
+
+  return (
+    <div>
+        <h2>Checkout</h2>
+        <form onSubmit={manejadorFormulario}>
+            {carrito.map(producto => (
+                <div>
+                    <p>
+                        {producto.item.nombre} x {producto.cantidad}
+                    </p>
+                    <p>
+                        Precio $ {producto.item.precio} </p>
+                    <hr />
+                </div>
+            ))}
+            <hr />
+                <div>
+                    <label htmlFor="nombre"> Nombre </label>
+                    <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="apellido"> Apellido </label>
+                    <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="telefono"> Telefono </label>
+                    <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor=""> Email </label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor=""> Email Confirmación </label>
+                    <input type="text" value={emailConfirmacion} onChange={(e) => setEmailConfirmacion(e.target.value)} />
+                </div>
+
+                {
+                    error &&   <p style={{color: "red"}}>{error}</p>
+                }
+                <button type="submit"> Finalizar Compra </button>
+        </form>
+        {
+            orderId && (
+            <strong> ¡Gracias por tu compra! Tu número de orden es {orderId} </strong>
+            )
+        }
+
+    </div>
+  )
+}
+
+export default Checkout

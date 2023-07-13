997c1a3fb7ba340853f52a5e4358ae770095cbe9
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-07-13 08:10:37 -0300

    pre firebase ok

---
+++ src/services/config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBujTCiHLr8gzAllVqZK0_dQVBLSKqaMwE",
+  authDomain: "base-de-datos---proyecto-react.firebaseapp.com",
+  projectId: "base-de-datos---proyecto-react",
+  storageBucket: "base-de-datos---proyecto-react.appspot.com",
+  messagingSenderId: "720037485733",
+  appId: "1:720037485733:web:65c13ffdcd55f9f1be014c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore (app);

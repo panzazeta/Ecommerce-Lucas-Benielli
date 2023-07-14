d88f12dbbf9086488897505f3120faeed8075049
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-07-14 18:02:35 -0300

    add correccion ok

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -26,13 +26,6 @@ const ItemListContainer = () => {
       .catch(error => console.log(error));
   }, [idCategoria])
 
-  // useEffect(() => {
-  //   const funcionProductos = idCategoria ? getProductosPorCategoria : getProductos;
-  //   funcionProductos(idCategoria)
-  //     .then(res => setProductos(res))
-  //     .catch(error => console.log(error))
-  // }, [idCategoria])
-
   return (
     <div>
       <h2>Catálogo</h2>

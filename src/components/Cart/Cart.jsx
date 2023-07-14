549a15367cba797798d7e69405bc29708a00f020
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-07-14 19:49:14 -0300

    estilos 4 ok

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import { CartContext } from "../../context/CartContext"
 import { Link } from "react-router-dom"
 import CartItem from "../CartItem/CartItem"
+import "./Cart.css";
 
 const Cart = () => {
     const {carrito, vaciarCarrito, total, cantidadTotal} = useContext (CartContext)
@@ -15,7 +16,7 @@ const Cart = () => {
         )
     }
   return (
-    <div>
+    <div className="cart-lista">
         {carrito.map(producto =>  <CartItem key={producto.id} {...producto} /> )}
         <h3> Total: $ {total} </h3>
         <h3> Cantidad Total : {cantidadTotal} </h3>

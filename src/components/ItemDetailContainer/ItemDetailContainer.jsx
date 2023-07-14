cac4a039c159762361ffe7049bb2fd7d7cf3d515
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-07-14 09:09:26 -0300

    agregado final 1 ok

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react"
-// import { getUnProducto } from "../../asyncmock"
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
 import { getDoc, doc } from "firebase/firestore"
@@ -20,13 +19,6 @@ const ItemDetailContainer = () => {
       })
       .catch(error => console.log(error))
   }, [idItem]);
-
-
-  // useEffect( () => {
-  //   getUnProducto(idItem)
-  //      .then(res => setProducto(res))
-  //      .catch(error => console.log(error))
-  // },[idItem]);
 
   return (
     <div>

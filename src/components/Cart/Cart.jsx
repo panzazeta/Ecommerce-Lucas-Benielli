270b1274fbb77936e234a7b286a64161eacf9b1c
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-07-17 07:49:10 -0300

    sigo estilos ok

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -19,7 +19,7 @@ const Cart = () => {
     <div className="cart-lista">
         {carrito.map(producto =>  <CartItem key={producto.id} {...producto} /> )}
         <h3> Cantidad Total : {cantidadTotal} libros</h3>
-        <h3> Total: $ {total} </h3>
+        <h3> Total: ${total} </h3>
         <button onClick= {() => vaciarCarrito()}> Vaciar carrito</button>
         <Link to="/checkout"> Finalizar Compra</Link>
     </div>
